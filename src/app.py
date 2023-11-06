49767d0b480be5c3153fe0a03e9fc9fc282bad1f
paulaechemendia <140556972+paulaechemendia@users.noreply.github.com>
2023-11-06 20:23:04 +0000

    HECHO

--- src/app.py
+++ src/app.py
@@ -2,14 +2,43 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
-
+import pandas as pd
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import matplotlib.pyplot as plt
 # load the .env file variables
 load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+artisturl = '1Xyo4u8uXC1ZmMpatF05PJ'
+client_credential_manager = SpotifyClientCredentials(client_id=os.getenv("CLIENT_ID"), client_secret=os.getenv("CLIENT_SECRET"))
+sp = spotipy.Spotify(client_credentials_manager=client_credential_manager)
+results = sp.artist_top_tracks(artisturl)
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+for track in results['tracks'][:10]:
+    print('track    : ' + str(track['name']))
+    print('popularity    : ' + str(track['popularity']))
+    print('duration: ' + str(track['duration_ms']/1000/60))
+    print()
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+songs = []
+for track in results['tracks'][:10]:
+    song = { 'Track' : str(track['name']),
+            'Popularity' :  track['popularity'], 
+            'Duration' : track['duration_ms']/1000/60           
+
+    }
+    songs.append(song)
+
+step6 = pd.DataFrame(songs[:3])
+print(step6)
+
+x = []
+y = []
+for track in results['tracks'][:10]:
+
+    x.append(track['popularity'])
+    y.append(track['duration_ms']/1000/60 )
+
+plt.scatter(x, y)
+plt.show()
